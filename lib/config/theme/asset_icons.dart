4b9c1cc5fc58e6dfb44a3395936940ad459335be
Kien <kien.dang@moonlysoftware.com>
2025-10-24 11:02:42 +0200

    feat: add new SVG icons for chevron-down, eye, and eye-slash

--- lib/config/theme/asset_icons.dart
+++ lib/config/theme/asset_icons.dart
@@ -1,7 +1,13 @@
 class AssetIcons {
   static const _path = 'assets/icons';
 
+  // newly added icons within the appthemes
   static const String addWidget = '$_path/add-widget.svg';
-  static const String settings = '$_path/settings.svg';
   static const String save = '$_path/save.svg';
+
+  // existing icons in the real app
+  static const String settings = '$_path/settings.svg';
+  static const String chevronDown = '$_path/chevron-down.svg';
+  static const String eyeSlash = '$_path/eye-slash.svg';
+  static const String eye = '$_path/eye.svg';
 }
