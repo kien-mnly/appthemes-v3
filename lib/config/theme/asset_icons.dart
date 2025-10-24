4661bcffb2818d7c13511e7c9c69008cfdabf03b
Kien <kien.dang@moonlysoftware.com>
2025-10-24 14:59:43 +0200

    feat: asset icons and configuration

--- lib/config/theme/asset_icons.dart
+++ lib/config/theme/asset_icons.dart
@@ -4,6 +4,14 @@ class AssetIcons {
   // newly added icons within the appthemes
   static const String addWidget = '$_path/add-widget.svg';
   static const String save = '$_path/save.svg';
+  static const String barChart = '$_path/bar-chart.svg';
+  static const String lineChart = '$_path/line-chart.svg';
+  static const String batteryFull = '$_path/battery-full.svg';
+  static const String batteryHealth = '$_path/battery-health.svg';
+  static const String coins = '$_path/coins.svg';
+  static const String plant = '$_path/plant.svg';
+  static const String smartMode = '$_path/smart-mode.svg';
+  static const String zinVoltLogo = '$_path/zinvolt-logo.svg';
 
   // existing icons in the real app
   static const String settings = '$_path/settings.svg';
